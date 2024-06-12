c605f7f042329df44fdf55411156d66fe9ef9d68
gkulkarni82 <kulk.gajanan@gmail.com>
2024-06-12 17:27:52 +0530

    Additional Snippet

--- Code_Snippet/code_snip2.cpp
+++ Code_Snippet/code_snip2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
 
 /*
@@ -156,7 +157,6 @@ int main()
 // 2. Now the object objBT is assigned to int pointer and change value to 23 and display the value will change it to --
 //  means 22
 
-*/
 
 //Snip 7
 class Point
@@ -195,3 +195,222 @@ int main()
 // 2. then objp2 will set x = 1 and y = 2
 // 3. operator overaloading will lead to adding these objects 
 // 4. making it 11 and 22
+
+
+
+//Snip 8
+class Bix
+{
+    int x, y; 
+    public:
+    void show(void);
+    void main(void);
+};
+void Bix::show(void)
+{ 
+    Bix b;
+    b.x = 2;
+    b.y = 4;
+    cout<< x << " " << y;
+}
+void Bix::main(void)
+{
+    Bix b;
+    b.x = 6; 
+    b.y = 8;
+    b.show();
+}
+int main(int argc, char *argv[])
+{
+    Bix run;
+    run.main();
+    return 0; 
+}
+// Output: 6 8
+// Explaination:
+// 1. Here in main new b object is created and assigned value as  6 and 8
+// 2. Now using b.show() function is called so the value of b is still 6 and 8
+// 3. in Show again we are creating new object as Bix b and setting the value as 2 and 4 this is new object with value 
+// 4. but while printing we are printing x and y which is local so the value will be 6 and 8 from the calling 
+// method Bix object
+
+//Snip 9
+int main()
+{
+    int x = 80; 
+    int &y = x;
+    x++;
+    cout << x << " " << --y;
+    return 0;
+}
+
+
+//Snip 10
+enum xyz 
+{
+    a, b, c
+}; 
+int main()
+{
+    int x = a, y = b, z = c; 
+    int &p = x, &q = y, &r = z; 
+    p = z; 
+    p = ++q;
+    q = ++p;
+    z = ++q + p++; 
+    cout<< p << " " << q << " " << z;
+    return 0; 
+}
+// Output: 4 4 7
+// Explaination:
+// 1.Enums has default value starts from 0 1 2 so a = 0 b = 1 c = 3
+// 2. these values are assigned to x , y,z respectively
+// 3. p = z  means = 2;
+// 4. p = ++q = 2
+// 5. q= ++p  means both p and q are at 3  3
+// 6. ++q + p++ means q = 4 and p = 3 z = 7 as p is post increament
+// 7. output after post increment is 4 4 7
+
+
+
+//Snip 11
+class IndiaBix
+{
+    int val; 
+    public:
+    void SetValue(char *str1, char *str2)
+    {
+        val = strcspn(str1, str2);
+    }
+    void ShowValue()
+    {
+        cout<< val;
+    } 
+};
+int main() 
+{
+    IndiaBix objBix;
+    objBix.SetValue((char*)"India", (char*)"Bix"); 
+    objBix.ShowValue(); 
+    return 0; 
+}
+
+// Output: 3
+// Explaination: strcspn is a function which returns the value till bot the string finds matching value 
+// 1. here India and Bix are not matching characters hence 3 value
+
+//Snip 12
+class IndiaBix
+{
+    int x; 
+    public:
+    IndiaBix(short ss)
+    {
+        cout<< "Short" << endl;
+    }
+    IndiaBix(int xx)
+    {
+        cout<< "Int" << endl;
+    }
+    IndiaBix(float ff)
+    {
+        cout<< "Float" << endl;
+    }
+    ~IndiaBix() 
+    {
+        cout<< "Final";
+    }
+};
+int main()
+{
+    IndiaBix *ptr = new IndiaBix('B');
+    return 0; 
+}
+
+// Output: int
+// Explaination: There are no constructor for char and the nearest matching constructor is the one accepting int
+// so Int will be printed
+
+//Snip 13
+class BixBase
+{   
+    public:
+    BixBase()
+    {
+        cout<< "Base OK. "; 
+    }
+    ~BixBase()
+    {
+        cout<< "Base DEL. "; 
+    }
+};
+class BixDerived: public BixBase
+{
+    public:
+    BixDerived()
+    { 
+        cout<< "Derived OK. "; 
+    }
+    ~BixDerived()
+    { 
+        cout<< "Derived DEL. "; 
+    }
+};
+int main()
+{
+    BixBase *basePtr = new BixDerived();
+    delete basePtr;
+    return 0;
+}
+
+// Output:  Base OK. Derived OK. Base DEL. 
+// Explaination:
+// 1. Pointer is of type Base so base constructor is called
+// 2. Pointer is created from Derived so derived constructor is called.
+// 3. Since base class do not have virutal destructor so only Base destructor will be called.
+*/
+
+//Snip 14
+// int main() {
+//    auto glambda = [](auto a, auto&& b) { return a < b; };
+//    bool b = glambda(3, 3.14);
+//    std::cout<<b;
+   
+//     return 0;
+// }
+
+// Output: 1
+// Explaination: it will output 1 as we are not using boolalpha to parase it to print True or false
+
+
+//Snip 15
+// class base
+// {
+// public:
+//        base()
+//        {          
+//            cout<<"BCon";
+//        }
+//        ~base()
+//        {
+// 	   cout<<"BDest ";
+//        }
+// };
+// class derived: public base
+// {
+// public:
+//        derived()
+//        {     cout<<"DCon ";
+//        }
+//        ~derived()
+//        {     cout<<"DDest ";
+//        }
+// };
+
+// int main()
+// {
+// derived object;
+// return 0; 
+// }
+
+// Output: BCon DCon DDest BDest
---
+++ Code_Snippet/pointer_snip.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+#include<stdio.h>
+using  namespace std;
+
+/*
+//Snip 1
+main()
+{
+    float a = 10.2;
+    int *p = a;
+    printf("%d",*p);
+}
+
+Output: compilation error not a compatible type
+Explaination: Cannot initialised int pointer with float
+
+
+//Snip 2
+main()
+{
+    int a[5] = {1, 2, 3, 4, 5};
+    int *p = a;
+    printf("%d\t%d\t%d\t%d\t",*p,0[a],a,p);
+}
+
+// Output : 1 1 Address address
+// Explaination:
+// 1.*p and 0[a] will access the value at base address which is 0th location 
+// 2.a and p will print the address
+*/
+
+// int arr[] = {1,2,3};
+// main()
+// {
+//     int *ptr; 
+//     ptr = arr;
+//     ptr = ptr + 3;
+//     printf("%d",*ptr);
+// } 
+
+// Output: 0
+// Explaination:
+
+// int main()
+// {
+//     int const *p = 5;
+//     printf("%d", ++(*p));
+// }
+
+// Output: Compilation error invalid conversion from int to int*
+//Explaination: Cannot modify a constant value. p is a pointer to a constant integer.
+// But we tried to change the value of the constant integer
+
+
+// int main()
+// {
+//     struct std
+//     {
+//         int x = 3;
+//         char name[] = "hello";
+//     };
+//     struct std *s;
+//     printf("%d", s->x);
+//     printf("%s", s->name);
+// } 
+
+// Output: hello 5
+// Explaination: error because of initializing variables in structure declaration
+
+
+int main()
+{
+    register i = 5;
+    char j[] = "hello";
+    printf("%s %d", j, i);
+}
+
+// Output: hello 5
+// Explaination: if you declare i as register compiler will treat it as ordinary integer and it will take integer value.
+// i value may be stored either in register or in memory
+
+
+
+// int fun(int *a,int *b)
+// {
+//     *a = *a+*b;
+//     *b = *a-*b;
+//     *a = *a-*b;
+// }
+
+// int main()
+// {
+//     int x = 10,y = 20;
+//     fun(&x,&y);
+//     printf("x= %d y = %d\n", x, y);
+// }
+
+// Output: x = 20 , y = 10

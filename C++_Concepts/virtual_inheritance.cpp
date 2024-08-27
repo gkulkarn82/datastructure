3e50f315d3d87b8d05c9e305334e963bc9b6f5d5
gkulkarn82 <kulk.gajanan@gmail.com>
2024-08-27 13:28:11 +0530

    Update comments and notes

--- C++_Concepts/virtual_inheritance.cpp
+++ C++_Concepts/virtual_inheritance.cpp
@@ -57,6 +57,16 @@ int main()
     // basePtr->print();
     // delete basePtr;
 
+       //Upcasting is allowed where this is a downcasting which is not allowed
+    //    Derived* derivedPtr = dynamic_cast<Derived*>(new Base());
+    //    if(derivedPtr == nullptr)
+    //    {
+    //         cout<< "This is null casting failed";
+    //         return 0;     
+    //    }
+    //    derivedPtr->print();
+    //    delete derivedPtr;
+
     /*
         O/P
         Print Of Derived Class 
@@ -99,12 +109,116 @@ int main()
         Derived will be called based on type of pointer
 
         Same is the case with destructor.
-    
-    
     */
 
-    Base* ptr = new Derived();
-    delete ptr;
-
+    // Base* ptr = new Derived();
+    // delete ptr;
     return 0;
 }
+
+
+/*
+    Detailed Notes :
+        As I keep forgetting things hence I decide to have a proper notes which I can read and which will give me the 
+        correct pointers when I am going through them
+
+    I : Without Virtualisation
+    class Emp
+    {
+        public:
+            void RaiseSalary()
+            {
+                //some code
+            }
+    }
+    class Engineer: public Emp
+    {
+        public:
+            void RaiseSalary()
+            {
+                //some code
+            }
+    }
+
+    int main()
+    {
+        Emp* emp = new Engineer();
+        emp->RaiseSalary(); 
+    }
+    
+    //In this situation the function will be called based on the type of object created i.e Emp and not based on constructor
+    //Hence the base class method will be called
+
+    II: With Virtualisation
+
+    class Emp
+    {
+        public:
+            virtual void RaiseSalary()
+            {
+                //some code
+            }
+    }
+    class Engineer: public Emp
+    {
+        public:
+            void RaiseSalary()
+            {
+                //some code
+            }
+    }
+
+    int main()
+    {
+        Emp* emp = new Engineer();
+        emp->RaiseSalary(); 
+    }
+
+    // Here the function is virtual hence the function will be called based on the constructor() and not based on type
+    //Reason
+
+    1.Every class has a vtable and vptr becase 
+        - if any class has at least one virtual method then all it's sucessing classes has vptr and vtable
+    2.vtable maintaince the table of function pointer and vptr points to it
+    3. Vtables are static arrays
+        - i.e All the object instances points to the same vtable
+
+    vtable for Emp
+    Emp* obj = new Emp();
+    this will create object of type Emp vptr will point to EMP vtable like below
+
+    --------------------------------------------------------------------------------------
+    | Sr   | Function Name   | Class      |  Comments                                    |
+    --------------------------------------------------------------------------------------
+    |  1   | RaiseSalary     | Emp        |  This is virutal function form Emp class     |
+    --------------------------------------------------------------------------------------
+    |  2   | Promote         | Emp        |  This is non virtual function from emp class |
+    --------------------------------------------------------------------------------------
+
+    vtable for Engineer
+    Emp* obj = new Engineer();
+    obj is object of type Emp and will point at Engineer vtable
+    here 
+    1. RaiseSalary will be pointed from Enginner class
+    2. Promote since it is non- virtual function based inherited hence will point to Emp class
+
+    ----------------------------------------------------------------------------------------------
+    | Sr   | Function Name   | Class      |  Comments                                            |
+    ----------------------------------------------------------------------------------------------
+    |  1   | RaiseSalary     | Engineer   |  This is function form Engineer class                |
+    ----------------------------------------------------------------------------------------------
+    |  2   | Promote         | Emp        |  This is non virtual but inherited function from Emp |
+    ----------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+*/
